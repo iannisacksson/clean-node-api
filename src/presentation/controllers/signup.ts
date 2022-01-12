d58a1f84f18ac82edac3e1205d3e5e8be5857472
Iann Isacksson <iisackssonoliveira@gmail.com>
2022-01-12 16:20:49 -0300

    refactor: move server error to a heloer method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,6 @@
 import { InvalidParamError } from '../errors/invalid-param-error';
 import { MissingParamError } from '../errors/missing-param-error';
-import { ServerError } from '../errors/server-error';
-import { badRequest } from '../helpers/http-helper';
+import { badRequest, serverError } from '../helpers/http-helper';
 import { IController } from '../protocols/controller';
 import { IEmailValidator } from '../protocols/email-validator';
 import { IHttpRequest, IHttpResponse } from '../protocols/http';
@@ -36,10 +35,7 @@ class SignUpController implements IController {
 
       return { statusCode: 200, body: {} };
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: new ServerError(),
-      };
+      return serverError();
     }
   }
 }
