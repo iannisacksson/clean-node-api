2bbb37861204b081ba0953228eac28b112ddd15b
Iann Isacksson <iisackssonoliveira@gmail.com>
2022-01-14 13:51:06 -0300

    test: ensure SignUpController return 400 if password confirmation fails

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -99,6 +99,26 @@ describe('SignUp Controller', () => {
     );
   });
 
+  test('should return 400 if password confirmation fails', () => {
+    const { signUpController } = makeSignUpController();
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'invalid_password',
+      },
+    };
+
+    const httpResponse = signUpController.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
+      new InvalidParamError('passwordConfirmation'),
+    );
+  });
+
   test('should return 400 if an invalid email is provided', () => {
     const { signUpController, emailValidatorStub } = makeSignUpController();
 
