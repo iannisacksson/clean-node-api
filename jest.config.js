b935433dce59bccff7b499cd2174508f18f38808
Iann Isacksson <iisackssonoliveira@gmail.com>
2022-01-15 11:56:41 -0300

    chore: add jest configs and scripts

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,6 @@ module.exports = {
   coverageReporters: ['text-summary', 'lcov'],
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/*.spec.ts'],
   transform: {
     '.+\\.ts$': 'ts-jest',
   },
