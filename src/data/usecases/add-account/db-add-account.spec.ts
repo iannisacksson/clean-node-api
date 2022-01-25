42a736ea66aad13faa088649a723ea1e55af998c
Iann Isacksson <iisackssonoliveira@gmail.com>
2022-01-25 14:19:02 -0300

    refactor: move dbAddAccount creation to a factory helper method

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,15 +1,27 @@
+import { IEncrypter } from '../../protocols/encrypter';
 import { DbAddAccount } from './db-add-account';
+
+interface ISutTypes {
+  dbAddAccount: DbAddAccount;
+  encrypterStub: IEncrypter;
+}
+
+const makeDbAddAccount = (): ISutTypes => {
+  class EncrypterStub {
+    async encrypt(): Promise<string> {
+      return 'hashed_password';
+    }
+  }
+
+  const encrypterStub = new EncrypterStub();
+  const dbAddAccount = new DbAddAccount(encrypterStub);
+
+  return { encrypterStub, dbAddAccount };
+};
 
 describe('DbAddAccount UseCase', () => {
   test('Should call Encrypter with correct password', async () => {
-    class EncrypterStub {
-      async encrypt(): Promise<string> {
-        return 'hashed_password';
-      }
-    }
-
-    const encrypterStub = new EncrypterStub();
-    const dbAddAccount = new DbAddAccount(encrypterStub);
+    const { dbAddAccount, encrypterStub } = makeDbAddAccount();
 
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt');
 
