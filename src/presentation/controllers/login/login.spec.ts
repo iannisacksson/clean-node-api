557f827e4915bd5eca0bbeb81dfa204d760b2483
Iann Isacksson <iann.oliveira@awscode.com.br>
2022-03-10 09:43:42 -0300

    test: ensure LoginController returns 400 if no email is provider

---
+++ src/presentation/controllers/login/login.spec.ts
@@ -0,0 +1,18 @@
+import { MissingParamError } from '../../errors';
+import { badRequest } from '../../helpers/http-helper';
+import { LoginController } from './login';
+
+describe('Login Controller', () => {
+  test('Should return 400 if no email is provided', async () => {
+    const loginController = new LoginController();
+    const httpRequest = {
+      body: {
+        passoword: 'any_password',
+      },
+    };
+
+    const httpResponse = await loginController.handle(httpRequest);
+
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')));
+  });
+});
