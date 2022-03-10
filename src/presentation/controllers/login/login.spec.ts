8b191733a5f5619f4948cb88bc0e1396ee8dad6e
Iann Isacksson <iann.oliveira@awscode.com.br>
2022-03-10 09:50:17 -0300

    refactor: move sut creation to a factory method

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -2,9 +2,21 @@ import { MissingParamError } from '../../errors';
 import { badRequest } from '../../helpers/http-helper';
 import { LoginController } from './login';
 
+interface ISutTypes {
+  loginController: LoginController;
+}
+
+const makeSut = (): ISutTypes => {
+  const loginController = new LoginController();
+
+  return {
+    loginController,
+  };
+};
+
 describe('Login Controller', () => {
   test('Should return 400 if no email is provided', async () => {
-    const loginController = new LoginController();
+    const { loginController } = makeSut();
     const httpRequest = {
       body: {
         passoword: 'any_password',
@@ -17,7 +29,20 @@ describe('Login Controller', () => {
   });
 
   test('Should return 400 if no password is provided', async () => {
-    const loginController = new LoginController();
+    const { loginController } = makeSut();
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+      },
+    };
+
+    const httpResponse = await loginController.handle(httpRequest);
+
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('password')));
+  });
+
+  test('Should return 400 if no password is provided', async () => {
+    const { loginController } = makeSut();
     const httpRequest = {
       body: {
         email: 'any_email@mail.com',
