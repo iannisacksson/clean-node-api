41bb60b5ced661c1550c35c72a4de50615ab71c4
Iann Isacksson <iann.oliveira@awscode.com.br>
2022-03-14 11:50:27 -0300

    feat: ensure LoginController calls email EmailValidator with correct email

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,15 @@
 import { MissingParamError } from '../../errors';
 import { badRequest } from '../../helpers/http-helper';
 import { IController, IHttpRequest, IHttpResponse } from '../../protocols';
+import { IEmailValidator } from '../signup/signup-protocols';
 
 export class LoginController implements IController {
+  private readonly emailValidator: IEmailValidator;
+
+  constructor(emailValidator: IEmailValidator) {
+    this.emailValidator = emailValidator;
+  }
+
   public async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
     if (!httpRequest.body.email) {
       return badRequest(new MissingParamError('email'));
@@ -11,6 +18,8 @@ export class LoginController implements IController {
     if (!httpRequest.body.password) {
       return badRequest(new MissingParamError('password'));
     }
+
+    this.emailValidator.isValid(httpRequest.body.email);
 
     return null;
   }
