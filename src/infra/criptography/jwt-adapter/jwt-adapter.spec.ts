cde9483202a34128b6d7989956d6fef9fd36a2e7
Iann Isacksson <iann.oliveira@awscode.com.br>
2022-04-02 12:05:54 -0300

    test: ensure JwtAdapter throw if sign throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -25,4 +25,16 @@ describe('Jwt Adapter', () => {
 
     expect(accessToken).toBe('any_token');
   });
+
+  test('Should throw if sign throws', async () => {
+    const jwtAdapter = new JwtAdapter('secret');
+
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    const promise = jwtAdapter.encrypt('any_id');
+
+    await expect(promise).rejects.toThrow();
+  });
 });
