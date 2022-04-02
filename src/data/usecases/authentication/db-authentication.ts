e37aaaa2c075147c856345f80661c697b3b876ad
Iann Isacksson <iann.oliveira@awscode.com.br>
2022-04-02 10:01:28 -0300

    feat: ensure DbAuthentication call HashComparer with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -2,17 +2,29 @@ import {
   IAuthenticationModel,
   IAuthentication,
 } from '../../../domain/usecases/authentication';
+import { IHashComparer } from '../../protocols/criptography/hash-comparer';
 import { ILoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository';
 
 export class DbAuthentication implements IAuthentication {
   private readonly loadAccountByEmailRepository: ILoadAccountByEmailRepository;
+  private readonly hashComparer: IHashComparer;
 
-  constructor(loadAccountByEmailRepository: ILoadAccountByEmailRepository) {
+  constructor(
+    loadAccountByEmailRepository: ILoadAccountByEmailRepository,
+    hashComparer: IHashComparer,
+  ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository;
+    this.hashComparer = hashComparer;
   }
 
   public async auth(data: IAuthenticationModel): Promise<string> {
-    await this.loadAccountByEmailRepository.load(data.email);
+    const account = await this.loadAccountByEmailRepository.load(data.email);
+
+    if (!account) {
+      return null;
+    }
+
+    await this.hashComparer.compare(data.password, account.password);
 
     return null;
   }
